09b2f06614248423a2f105f7c1f71bca2161bae3
Dominic Fam <famdominic1218@gmail.com>
2024-11-12 14:14:43 +0800

    commit

--- src/components/list.js
+++ src/components/list.js
@@ -1,14 +1,11 @@
 import TodoItem from "./item";
 
 function TodoList(props) {
-    const { todos } = props;
+  const { todos } = props;
   return (
     <ul className="list-group">
       {todos.map((item) => {
-        return <TodoItem 
-            text={item.text}
-            isCompleted={item.isCompleted} 
-            />;
+        return <TodoItem text={item.text} isCompleted={item.isCompleted} />;
       })}
     </ul>
   );
